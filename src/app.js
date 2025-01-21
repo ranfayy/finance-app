d997351be51e4d10864a6abd3b2908fa5b01deb6
Rofi Dzaki Abdul Aziz <rofidzakismktibazma@gmail.com>
2025-01-21 11:18:28 +0700

    Memanggil financeRoutes ke app

--- src/app.js
+++ src/app.js
@@ -9,8 +9,10 @@ const app = express();
 app.use(express.json());
 
 const userRoutes = require('./routes/userRoutes');
+const financeRoutes = require('./routes/financeRoutes');
 
 app.use('/api/users', userRoutes);
+app.use('/api/finances', financeRoutes);
 
 const PORT = process.env.PORT;
 
