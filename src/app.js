ed1e3f73bc01a3544b2494f02ad1d346760ed6b8
Rofi Dzaki Abdul Aziz <rofidzakismktibazma@gmail.com>
2025-01-21 11:03:23 +0700

    Memanggil userRoutes ke app

--- src/app.js
+++ src/app.js
@@ -8,6 +8,10 @@ const app = express();
 
 app.use(express.json());
 
+const userRoutes = require('./routes/userRoutes');
+
+app.use('/api/users', userRoutes);
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`Server berjalan di port ${PORT}`));
